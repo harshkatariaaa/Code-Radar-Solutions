58aafe865e8874146deba3829dd7717fc7a59f34
harshkatariaaa <harshkataria090@gmail.com>
2025-04-09 11:48:15 +0530

    Solution to Arcade Leaderboard -- Track Your Rank

--- Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
+++ Arcade Leaderboard -- Track Your Rank@@67de00956ac3fec9afb90e1b/code.c
@@ -1,46 +1,52 @@
 #include <stdio.h>
 
+// Function prototype
+void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]);
+
 int remove_duplicates(int arr[], int n, int unique[]) {
-    int i, j = 0;
+    int j = 0;
     unique[j++] = arr[0];
-    for (i = 1; i < n; i++) {
+    for (int i = 1; i < n; i++) {
         if (arr[i] != arr[i - 1]) {
             unique[j++] = arr[i];
         }
     }
-    return j; // return new size
+    return j;
 }
 
- {
-    int n, m;
-    scanf("%d", &n);
-
-    int leaderboard[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &leaderboard[i]);
-    }
-
-    // Create unique leaderboard (Dense Ranking)
+void trackPlayerRanks(int ranked[], int n, int player[], int m, int result[]) {
     int unique[n];
-    int unique_size = remove_duplicates(leaderboard, n, unique);
-
-    scanf("%d", &m);
-    int scores[m];
-    for (int i = 0; i < m; i++) {
-        scanf("%d", &scores[i]);
-    }
-
+    int unique_size = remove_duplicates(ranked, n, unique);
     int index = unique_size - 1;
 
     for (int i = 0; i < m; i++) {
-        int player_score = scores[i];
-
-        // Move up leaderboard while player_score >= current leaderboard score
-        while (index >= 0 && player_score >= unique[index]) {
+        int score = player[i];
+        while (index >= 0 && score >= unique[index]) {
             index--;
         }
+        result[i] = index + 2;
+    }
+}
 
-        printf("%d\n", index + 2); // index+2 due to 0-based indexing
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    int ranked[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &ranked[i]);
+    }
+
+    scanf("%d", &m);
+    int player[m];
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &player[i]);
+    }
+
+    int result[m];
+    trackPlayerRanks(ranked, n, player, m, result);
+
+    for (int i = 0; i < m; i++) {
+        printf("%d\n", result[i]);
     }
 
     return 0;
